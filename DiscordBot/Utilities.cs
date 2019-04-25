28cd377b9363739df73b22480a34718ff7f41856
Даниил <danyl1607@gmail.com>
2019-04-25 18:07:36 +0300

    Created unit tests and moved solution

---
+++ DiscordBot/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot
+{
+    public class Utilities
+    {
+        public static int MyUtility(int i)
+        {
+            return i;
+        }
+    }
+}
