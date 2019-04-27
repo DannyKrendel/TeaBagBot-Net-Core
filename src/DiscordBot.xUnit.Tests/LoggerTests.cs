271b529d96d7917e5dfc0c586e0bd39c79f5c582
Даниил <danyl1607@gmail.com>
2019-04-28 01:02:54 +0300

    Added Logger tests

---
+++ src/DiscordBot.xUnit.Tests/LoggerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace DiscordBot.xUnit.Tests
+{
+    public class LoggerTests
+    {
+        [Fact]
+        public void BasicLogger_ThrowsException()
+        {
+            var logger = Unity.Resolve<ILogger>();
+
+            Assert.Throws<ArgumentException>(() => logger.Log(null));
+        }
+    }
+}
