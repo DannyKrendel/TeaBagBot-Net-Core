b59d0ca2dad7a64d8ed1453af47dde9b2b889900
Даниил <danyl1607@gmail.com>
2019-04-29 22:44:23 +0300

    Update unit tests

---
+++ src/DiscordBot.xUnit.Tests/CommandServiceConfigFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using DiscordBot.Core;
+using Xunit;
+
+namespace DiscordBot.xUnit.Tests
+{
+    public class CommandServiceConfigFactoryTests
+    {
+        [Fact]
+        public void GetDefault_ShouldWork()
+        {
+            var config = CommandServiceConfigFactory.GetDefault();
+
+            Assert.NotNull(config);
+        }
+
+        [Fact]
+        public void GetNew_ShouldWork()
+        {
+            var config = CommandServiceConfigFactory.GetNew();
+
+            Assert.NotNull(config);
+        }
+    }
+}
---
+++ src/DiscordBot.xUnit.Tests/TokenManagerTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace DiscordBot.xUnit.Tests
+{
+    public class TokenManagerTests
+    {
+        [Fact]
+        public void GetToken_ShouldWork()
+        {
+            string token = TokenManager.GetToken();
+
+            Assert.NotNull(token);
+        }
+    }
+}
