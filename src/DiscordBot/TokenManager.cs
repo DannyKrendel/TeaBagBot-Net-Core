8a9b7db601e0717dff4f4963dc3b498a9c0b9494
Даниил <danyl1607@gmail.com>
2019-04-30 02:22:28 +0300

    Update TokenManager.cs

--- src/DiscordBot/TokenManager.cs
+++ src/DiscordBot/TokenManager.cs
@@ -5,7 +5,7 @@ namespace DiscordBot
 {
     public static class TokenManager
     {
-        private static readonly string tokenPath = $@"C:\Users\Danny\Source\Repos\DiscordBot\src\DiscordBot\Config\Token";
+        private static readonly string tokenPath = @"C:\Users\Danny\Source\Repos\DiscordBot\src\DiscordBot\Config\Token";
         
         public static string GetToken()
         {
