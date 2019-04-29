d84eec9c46f77cb62ae22931aaf888f375b6d05d
Даниил <danyl1607@gmail.com>
2019-04-29 18:15:17 +0300

    Create CommandHandler.cs

---
+++ src/DiscordBot/Core/CommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Core
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient client;
+        private readonly CommandService service;
+
+        public CommandHandler(DiscordSocketClient client, CommandService service)
+        {
+            this.client = client;
+            this.service = service;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await service.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+        }
+    }
+}
