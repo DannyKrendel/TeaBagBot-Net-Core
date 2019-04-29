860f636a4ccd79a1e7eb8b48f1a58613ef850738
Даниил <danyl1607@gmail.com>
2019-04-29 20:42:13 +0300

    Rearrange tests

---
+++ src/DiscordBot.xUnit.Tests/JsonStorageTests.cs
@@ -0,0 +1,69 @@
+﻿using DiscordBot.Storage.Implementations;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace DiscordBot.xUnit.Tests
+{
+    public class JsonStorageTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("", null)]
+        public void JsonStorage_StoreObject_ShouldThrow(object obj, string path)
+        {
+            var mockFileSystem = new MockFileSystem();
+            var mockInputFile = new MockFileData("\"test\"");
+            mockFileSystem.AddFile(@"C:\test.json", mockInputFile);
+            var storage = new JsonStorage(mockFileSystem);
+
+            var exception = Record.Exception(() => storage.StoreObject(obj, path));
+
+            Assert.NotNull(exception);
+        }
+
+        [Theory]
+        [InlineData(@"C:\test", "test", "\"test\"")]
+        public void JsonStorage_StoreObject_ShouldWork(string path, string data, string expected)
+        {
+            var mockFileSystem = new MockFileSystem();
+            var storage = new JsonStorage(mockFileSystem);
+
+            storage.StoreObject(data, path);
+
+            Assert.True(Path.IsPathFullyQualified(path));
+            Assert.True(mockFileSystem.FileExists($"{path}.json"));
+            Assert.Equal(mockFileSystem.File.ReadAllText($"{path}.json"), expected);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void JsonStorage_RestoreObject_ShouldThrow(string path)
+        {
+            var mockFileSystem = new MockFileSystem();
+            var mockInputFile = new MockFileData("\"test\"");
+            mockFileSystem.AddFile(@"C:\test.json", mockInputFile);
+            var storage = new JsonStorage(mockFileSystem);
+
+            var exception = Record.Exception(() => storage.RestoreObject<string>(path));
+
+            Assert.NotNull(exception);
+        }
+
+        [Theory]
+        [InlineData(@"C:\test", "\"test\"", "test")]
+        public void JsonStorage_RestoreObject_ShouldWork(string path, string jsonData, string expected)
+        {
+            var mockFileSystem = new MockFileSystem();
+            var mockInputFile = new MockFileData(jsonData);
+            mockFileSystem.AddFile($"{path}.json", mockInputFile);
+            var storage = new JsonStorage(mockFileSystem);
+
+            var actual = storage.RestoreObject<string>(path);
+
+            Assert.True(Path.IsPathFullyQualified(path));
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/DiscordBot.xUnit.Tests/MemoryStorageTests.cs
@@ -0,0 +1,34 @@
+﻿using DiscordBot.Storage.Implementations;
+using DiscordBot.Storage.Interfaces;
+using Xunit;
+
+namespace DiscordBot.xUnit.Tests
+{
+    public class MemoryStorageTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        public void MemoryStorage_RestoreObject_ShouldWork(string expectedObj, string expectedKey)
+        {
+            IDataStorage storage = new MemoryStorage();
+            storage.StoreObject(expectedObj + "some string", expectedKey);
+            storage.StoreObject(expectedObj, expectedKey);
+
+            var actualObj = storage.RestoreObject<string>(expectedKey);
+
+            Assert.Equal(expectedObj, actualObj);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void MemoryStorage_RestoreObject_ShouldThrow(string key)
+        {
+            IDataStorage storage = new MemoryStorage();
+
+            var exception = Record.Exception(() => storage.RestoreObject<string>(key));
+
+            Assert.NotNull(exception);
+        }
+    }
+}
