2336760383642f46033b6dcc11522e6ad9e6f02f
Даниил <danyl1607@gmail.com>
2019-04-29 18:15:11 +0300

    Create CommandServiceConfigFactory.cs

---
+++ src/DiscordBot/Core/Factories/CommandServiceConfigFactory.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot.Core
+{
+    public static class CommandServiceConfigFactory
+    {
+        public static CommandServiceConfig GetDefault()
+        {
+            return new CommandServiceConfig()
+            {
+                CaseSensitiveCommands = false,
+                LogLevel = LogSeverity.Verbose
+            };
+        }
+
+        public static CommandServiceConfig GetNew()
+        {
+            return new CommandServiceConfig();
+        }
+    }
+}
