ca451a5e9b98860d6d35f4ede1ff74eccfce6f07
Даниил <danyl1607@gmail.com>
2019-04-29 22:46:22 +0300

    Change token path

--- src/DiscordBot/TokenManager.cs
+++ src/DiscordBot/TokenManager.cs
@@ -1,15 +1,12 @@
 ﻿using DiscordBot.Storage.Interfaces;
 using System;
-using System.IO;
-
 
 namespace DiscordBot
 {
     public static class TokenManager
     {
-        private static readonly string tokenPath = 
-            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Config\Token");
-
+        private static readonly string tokenPath = $@"C:\Users\Danny\Source\Repos\DiscordBot\src\DiscordBot\Config\Token";
+        
         public static string GetToken()
         {
             return Unity.Resolve<IDataStorage>().RestoreObject<string>(tokenPath);
