7f082400118014c21df491ce3cb10e427002b0f2
Danny <danyl1607@gmail.com>
2019-05-01 01:48:03 +0300

    Create ConnectionException.cs

---
+++ src/DiscordBot/Core/ConnectionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Core
+{
+    public class ConnectionException : Exception
+    {
+        public ConnectionException(string message) : base(message)
+        {
+
+        }
+
+        public ConnectionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
