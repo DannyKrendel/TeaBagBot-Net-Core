90fbcdb6b988bf1130a77a6f91fabc594e92db8e
Даниил <danyl1607@gmail.com>
2019-04-30 06:59:05 +0300

    Create PathExtensions.cs

---
+++ src/DiscordBot/Extensions/PathExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.IO.Abstractions;
+
+namespace DiscordBot.Extensions
+{
+    public static class PathExtensions
+    {
+        public static bool IsPathFullyQualified(this IPath thisPath, string path)
+        {
+            return Path.IsPathFullyQualified(path);
+        }
+    }
+}
