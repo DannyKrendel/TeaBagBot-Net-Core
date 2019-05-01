e6347e8a02e8ac84543e889c85a12e97b2ca5d7f
Danny <danyl1607@gmail.com>
2019-05-01 22:22:48 +0300

    Update LoggerTests.cs

--- src/DiscordBot.xUnit.Tests/LoggerTests.cs
+++ src/DiscordBot.xUnit.Tests/LoggerTests.cs
@@ -6,19 +6,9 @@ namespace DiscordBot.xUnit.Tests
     public class LoggerTests
     {
         [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        public void Log_ShouldThrowArgumentException_IfInvalidMessage(string msg)
-        {
-            var logger = new Logger();
-
-            var ex = Record.Exception(() => logger.Log(msg));
-
-            Assert.IsAssignableFrom<ArgumentException>(ex);
-        }
-
-        [Theory]
         [InlineData("test")]
+        [InlineData("")]
+        [InlineData(null)]
         public void Log_ShouldLog_IfValidMessage(string msg)
         {
             var logger = new Logger();
