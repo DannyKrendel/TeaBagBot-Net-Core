eda05b94374bbbf653a2800462006440ba63bf3c
Danny <danyl1607@gmail.com>
2019-05-01 16:44:45 +0300

    Create Misc.cs

---
+++ src/DiscordBot/Core/Modules/Misc.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Core.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            await Context.Channel.SendMessageAsync(message);
+        }
+    }
+}
