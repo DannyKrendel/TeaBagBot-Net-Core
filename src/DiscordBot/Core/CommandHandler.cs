8bc212988b96cc34a4fe65e3edcd5322e7a95fac
Danny <danyl1607@gmail.com>
2019-05-01 18:37:55 +0300

    Update CommandHandler.cs

--- src/DiscordBot/Core/CommandHandler.cs
+++ src/DiscordBot/Core/CommandHandler.cs
@@ -23,31 +23,15 @@ namespace DiscordBot.Core
         public async Task InitializeAsync()
         {
             await service.AddModulesAsync(Assembly.GetEntryAssembly(), null);
-
             client.MessageReceived += HandleMessageAsync;
         }
 
         public async Task HandleMessageAsync(SocketMessage socketMsg)
         {
-            if (socketMsg == null)
-                throw new ArgumentNullException(nameof(socketMsg), "Socket message was null.");
+            BotConfig config = ConfigManager.LoadConfig(); // this should throw
+
             var msg = socketMsg as SocketUserMessage;
-            if (msg == null)
-                throw new InvalidCastException($"Can't cast '{socketMsg.GetType()}' to '{typeof(SocketUserMessage)}'");
-
             int argPos = 0;
-
-            BotConfig config = null;
-
-            // This is here for debug purposes, and it does catch an exception
-            try
-            {
-                config = ConfigManager.LoadConfig();
-            }
-            catch
-            {
-                throw;
-            }
 
             if (!msg.HasStringPrefix(config.Prefix, ref argPos) &&
                 !msg.HasMentionPrefix(client.CurrentUser, ref argPos))
