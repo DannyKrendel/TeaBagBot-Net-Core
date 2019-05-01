386e13f89a275579de97325bc61034cf80de629f
Danny <danyl1607@gmail.com>
2019-05-01 18:37:42 +0300

    Update CommandHandlerTests.cs

--- src/DiscordBot.xUnit.Tests/CommandHandlerTests.cs
+++ src/DiscordBot.xUnit.Tests/CommandHandlerTests.cs
@@ -7,14 +7,6 @@ namespace DiscordBot.xUnit.Tests
 {
     public class CommandHandlerTests
     {
-        [Fact]
-        public async Task HandleMessage_ShouldThrowArgumentNullException_IfSocketMessageIsNull()
-        {
-            var comHandler = Unity.Resolve<CommandHandler>();
-
-            var argException = await Record.ExceptionAsync(async () => await comHandler.HandleMessageAsync(null));
-
-            Assert.IsType<ArgumentNullException>(argException);
-        }
+        
     }
 }
