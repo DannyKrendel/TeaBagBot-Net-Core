efe445764370ceb2e79fe931c58a4ea0e30c0d50
Danny <danyl1607@gmail.com>
2019-05-03 06:28:54 +0300

    Rename Misc to MainModule and add more commands

---
+++ src/DiscordBot/Core/Modules/MainModule.cs
@@ -0,0 +1,31 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Core.Modules
+{
+    public class MainModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly EmbedService embedService;
+        private readonly CommandManager commandManager;
+
+        public MainModule(EmbedService embedService, CommandManager commandManager)
+        {
+            this.embedService = embedService;
+            this.commandManager = commandManager;
+        }
+
+        [Command("help")]
+        public async Task Help()
+        {
+            await ReplyAsync("Помощь уже в пути.");
+        }
+
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = embedService.GetInfoEmbed("Echoed Message", message);
+
+            await ReplyAsync(embed: embed);
+        }
+    }
+}
