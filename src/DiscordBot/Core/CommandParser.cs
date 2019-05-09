b11fd8e7aa89c26a194869f35c91695e9d69211b
Danny <danyl1607@gmail.com>
2019-05-09 23:37:22 +0300

    Create CommandParser.cs

---
+++ src/DiscordBot/Core/CommandParser.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+
+namespace DiscordBot.Core
+{
+    public class CommandParser
+    {
+        public string Parse(string str, ICommandContext context)
+        {
+            string result = str;
+
+            if (str.Contains("$username"))
+            {
+                result = str.Replace("$username", context.User.Username);
+            }
+            else if (str.Contains("$usermention"))
+            {
+                result = str.Replace("$usermention", context.User.Mention);
+            }
+
+            return result;
+        }
+    }
+}
