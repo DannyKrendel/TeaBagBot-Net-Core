51ecd17a1b5282b4c2b367ba0f16d42038669df2
Danny <danyl1607@gmail.com>
2019-05-09 21:23:12 +0300

    Add ConsoleHandler to write console commands for bot to execute

---
+++ src/DiscordBot/ConsoleHandler.cs
@@ -0,0 +1,40 @@
+﻿using DiscordBot.Core.Logging;
+using DiscordBot.Core.Logging.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public class ConsoleHandler
+    {
+        private readonly ILogger logger;
+
+        public ConsoleHandler(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task CheckMessagesAsync()
+        {
+            string msg = "";
+
+            await Task.Run(() =>
+            {
+                do
+                {
+                    msg = Console.ReadLine();
+
+                    if (msg == "exit")
+                        break;
+
+                    HandleMessage(msg);
+                } while (true);
+            });
+        }
+
+        public void HandleMessage(string msg)
+        {
+            logger.Log(new BotLogMessage(BotLogSeverity.Info, "Console", msg));
+        }
+    }
+}
