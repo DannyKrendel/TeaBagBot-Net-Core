a95b73484ee4b8d3862125ae9ef3ce756965864e
Danny <danyl1607@gmail.com>
2020-07-04 07:51:59 +0300

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -8,7 +8,7 @@ $target = '-target:C:\Program Files\dotnet\dotnet.exe'
 $targetArgs = '-targetargs:"test src -c:' + $buildConfig + ' --logger:trx;LogFileName=results.trx /p:DebugType=full"' 
 $filter = '-filter:+[TeaBagBot*]*-[TeaBagBot*]*Tests.*'
 $output = '-output:' + $buildFolder + '\coverage.xml'
-$register = '-register:administrator'
+$register = '-register'
 
 # Run OpenCover with all params
 & $openCover $target $targetArgs $filter $register '-oldStyle' '-mergeoutput' $output
